2ae6e2b5263ef7e59235f64bf0977057a3ac45fc
Jeff Hykin <jeff.hykin@gmail.com>
2019-09-28 19:04:44 -0500

    _h-box and v-box

---
+++ src/h-box.vue
@@ -0,0 +1,12 @@
+<template functional>
+    <div class="h-box">
+        <slot></slot>
+    </div>
+</template>
+<style scoped>
+.h-box {
+    display: flex;
+    flex-direction: row;
+    justify-content: center;
+}
+</style>
---
+++ src/v-box.vue
@@ -0,0 +1,12 @@
+<template functional>
+    <div class="v-box">
+        <slot></slot>
+    </div>
+</template>
+<style scoped>
+.v-box {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+}
+</style>
